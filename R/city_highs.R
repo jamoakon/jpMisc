b648e07cd10a32091615c3fed418e26c176a119a
Jean-Pierre Amoakon <jamoakon@georgetowncollege.edu>
2019-05-01 13:32:37 -0400

    ready to release

--- R/city_highs.R
+++ R/city_highs.R
@@ -8,16 +8,9 @@
 #' city_highs("Auckland")
 #' city_highs("Mumbai")
 #' @import ggplot2
-#' @importFrom plyr mapvalues
-#' @importFrom mosaicData Weather
 #' @export
 city_highs <- function(city_name) {
-  month_name1 <-
-    plyr::mapvalues(Weather$month, from = 1:12,
-                    to = month.abb)
-  month_name2 <- factor(month_name1, levels = month.abb)
-  Weather$month_name <- month_name2
-  ggplot(subset(Weather, city == city_name), aes(x = month_name, y = high_temp)) +
+  ggplot(subset(weather, city == city_name), aes(x = month_name, y = high_temp)) +
     geom_violin(fill = "burlywood") +
     geom_jitter(width = 0.25, size = 0.3, alpha = 0.5) +
     labs(x = "Month", y = "High Temperature",
